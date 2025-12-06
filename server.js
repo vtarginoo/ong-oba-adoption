a1aba22994830447334b2622b50dc1222bdc5bea
vtarginoo <vtarginoo>
2025-12-06 19:28:32 -0300

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const next = require("next");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http.createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log("Next.js rodando na porta " + port);
+  });
+});
